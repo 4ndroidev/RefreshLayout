51ee51c13b98080377eede6338678868ceda4057
4ndoridev <4ndroidev@gmail.com>
2017-09-21 12:31:09 +0800

    初始化代码

---
+++ app/src/main/java/com/androidev/refreshlayout/RefreshHeader.java
@@ -0,0 +1,175 @@
+package com.androidev.refreshlayout;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+/*
+ * 标版默认下拉刷新头部
+ */
+public class RefreshHeader extends LinearLayout implements RefreshLayout.RefreshUIHandler {
+
+    private final static int FLIP_DURATION = 150;
+    private final static int ROTATE_DURATION = 2000;
+    private final static String LAST_UPDATE_TIME_FORMAT = "MM-dd HH:mm:ss";
+
+
+    private String pullMessage = "下拉以刷新...";
+    private String releaseMessage = "松开以刷新...";
+    private String refreshMessage = "正在刷新中...";
+    private String completeMessage = "刷新完成";
+
+    private int tintColor = Color.parseColor("#ff666666");
+    private long lastUpdateTime = System.currentTimeMillis();
+    private String lastUpdateKey = null;
+
+    private RotateAnimation mFlipAnimation;
+    private RotateAnimation mReverseFlipAnimation;
+    private RotateAnimation mRotateAnimation;
+    private TextView mTitle;
+    private TextView mTimestamp;
+    private ImageView mIndicator;
+    private int widthMeasureSpec;
+    private int heightMeasureSpec;
+
+    private int a;
+
+    public RefreshHeader(Context context) {
+        this(context, null);
+    }
+
+    public RefreshHeader(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RefreshHeader(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        a = context.getResources().getDisplayMetrics().widthPixels / 60;
+        setWillNotDraw(false);
+        setOrientation(VERTICAL);
+        setGravity(Gravity.CENTER);
+        setBackgroundColor(Color.YELLOW);
+        initViews(context);
+        buildAnimation();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        this.widthMeasureSpec = widthMeasureSpec;
+        this.heightMeasureSpec = heightMeasureSpec;
+    }
+
+    protected void initViews(Context context) {
+        setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ((int) (7.5 * a))));
+        LinearLayout childLayout = new LinearLayout(context);
+        childLayout.setOrientation(LinearLayout.HORIZONTAL);
+        childLayout.setGravity(Gravity.CENTER);
+        mIndicator = new ImageView(context);
+        mIndicator.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        mIndicator.setLayoutParams(new LayoutParams(4 * a, 4 * a));
+        childLayout.addView(mIndicator);
+        mTitle = new TextView(context);
+        mTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX, 2.33f * a);
+        mTitle.setPadding(2 * a, 0, 0, 0);
+        mTitle.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        childLayout.addView(mTitle);
+        childLayout.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        addView(childLayout);
+        mTimestamp = new TextView(context);
+        mTimestamp.setTextSize(TypedValue.COMPLEX_UNIT_PX, 1.83f * a);
+        mTimestamp.setPadding(0, a, 0, 0);
+        mTimestamp.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        mTimestamp.setVisibility(GONE);
+        addView(mTimestamp);
+    }
+
+    private void buildAnimation() {
+        mFlipAnimation = new RotateAnimation(0, -180, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        mFlipAnimation.setInterpolator(new LinearInterpolator());
+        mFlipAnimation.setDuration(FLIP_DURATION);
+        mFlipAnimation.setFillAfter(true);
+        mReverseFlipAnimation = new RotateAnimation(-180, 0, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        mReverseFlipAnimation.setInterpolator(new LinearInterpolator());
+        mReverseFlipAnimation.setDuration(FLIP_DURATION);
+        mReverseFlipAnimation.setFillAfter(true);
+        mRotateAnimation = new RotateAnimation(0, 360, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        mRotateAnimation.setInterpolator(new LinearInterpolator());
+        mRotateAnimation.setDuration(ROTATE_DURATION);
+        mRotateAnimation.setRepeatCount(Animation.INFINITE);
+        mRotateAnimation.setRepeatMode(Animation.INFINITE);
+        mRotateAnimation.setFillAfter(false);
+    }
+
+    private void updateLayout() {
+        measure(widthMeasureSpec, heightMeasureSpec);
+        layout(getLeft(), getTop(), getRight(), getBottom());
+    }
+
+    @Override
+    public void onPrepare() {
+        mTitle.setText(pullMessage);
+        mIndicator.setVisibility(VISIBLE);
+        mIndicator.setRotation(0);
+        mIndicator.setImageResource(R.drawable.refresh_header_arrow);
+        mTimestamp.setText("最后更新时间: " + new SimpleDateFormat(LAST_UPDATE_TIME_FORMAT).format(new Date(lastUpdateTime)));
+        updateLayout();
+    }
+
+    @Override
+    public void onStart() {
+        mTitle.setText(refreshMessage);
+        mIndicator.setVisibility(VISIBLE);
+        mIndicator.setRotation(0);
+        mIndicator.setImageResource(R.drawable.refresh_header_loading);
+        mIndicator.clearAnimation();
+        mIndicator.startAnimation(mRotateAnimation);
+        updateLayout();
+    }
+
+    @Override
+    public void onComplete() {
+        mTitle.setText(completeMessage);
+        mIndicator.clearAnimation();
+        mIndicator.setVisibility(GONE);
+        refresh = false;
+        updateLayout();
+        lastUpdateTime = System.currentTimeMillis();
+    }
+
+    private boolean refresh;
+
+    @Override
+    public void onPull(boolean willRefresh, int offset) {
+        if (refresh && !willRefresh) {
+            mTitle.setText(pullMessage);
+            mIndicator.clearAnimation();
+            mIndicator.startAnimation(mReverseFlipAnimation);
+            updateLayout();
+        } else if (!refresh && willRefresh) {
+            mTitle.setText(releaseMessage);
+            mIndicator.clearAnimation();
+            mIndicator.startAnimation(mFlipAnimation);
+            updateLayout();
+        }
+        refresh = willRefresh;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+    }
+}
---
+++ app/src/main/java/com/androidev/refreshlayout/RefreshLayout.java
@@ -0,0 +1,408 @@
+package com.androidev.refreshlayout;
+
+import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.Transformation;
+import android.widget.AbsListView;
+
+/**
+ * refer to {@link android.support.v4.widget.SwipeRefreshLayout}
+ */
+
+public class RefreshLayout extends ViewGroup {
+
+    private static final float DRAGGING_RESISTANCE = 2f;
+    private static final float DECELERATE_INTERPOLATION_FACTOR = 2f;
+    private static final float RATIO_OF_HEADER_HEIGHT_TO_REFRESH = 1.2f;
+
+    private View mContent;
+    private View mHeader;
+
+    private int mFrom;
+    private int mTouchSlop;
+    private int mTotalOffset;
+    private int mHeaderHeight;
+    private int mRefreshThreshold;
+    private int mActivePointerId = MotionEvent.INVALID_POINTER_ID;
+
+    private float mLastMotionX;
+    private float mLastMotionY;
+
+    private boolean canRefresh;
+    private boolean isRefreshing;
+    private boolean isBeingDragged;
+
+    private OnRefreshListener mListener;
+    private RefreshUIHandler mUIHandler;
+
+    private final Interpolator mDecelerateInterpolator = new DecelerateInterpolator(DECELERATE_INTERPOLATION_FACTOR);
+
+    private Animation mAnimateToStartPosition = new Animation() {
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            moveToStart(interpolatedTime);
+        }
+    };
+
+    private Animation mAnimateToRefreshPosition = new Animation() {
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            moveToRefresh(interpolatedTime);
+        }
+    };
+
+    public RefreshLayout(Context context) {
+        this(context, null);
+    }
+
+    public RefreshLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RefreshLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        setWillNotDraw(false);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        mAnimateToStartPosition.setInterpolator(mDecelerateInterpolator);
+        mAnimateToRefreshPosition.setInterpolator(mDecelerateInterpolator);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+        if (mHeader != null) {
+            mHeaderHeight = mHeader.getMeasuredHeight();
+            mRefreshThreshold = (int) (mHeaderHeight * RATIO_OF_HEADER_HEIGHT_TO_REFRESH);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        updateLayout();
+    }
+
+    private void updateLayout() {
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        if (mHeader != null) {
+            int left = paddingLeft;
+            int top = mTotalOffset + paddingTop - mHeaderHeight;
+            int right = left + mHeader.getMeasuredWidth();
+            int bottom = top + mHeaderHeight;
+            mHeader.layout(left, top, right, bottom);
+        }
+        if (mContent != null) {
+            int left = paddingLeft;
+            int top = mTotalOffset + paddingTop;
+            int right = left + mContent.getMeasuredWidth();
+            int bottom = top + mContent.getMeasuredHeight();
+            mContent.layout(left, top, right, bottom);
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (!isEnabled() || mContent == null || mHeader == null || canChildScrollUp())
+            return false;
+        final int action = MotionEventCompat.getActionMasked(ev);
+        int pointerIndex;
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mActivePointerId = ev.getPointerId(0);
+                isBeingDragged = false;
+                canRefresh = !isRefreshing;
+                pointerIndex = ev.findPointerIndex(mActivePointerId);
+                if (pointerIndex < 0) {
+                    return false;
+                }
+                mLastMotionX = ev.getX(pointerIndex);
+                mLastMotionY = ev.getY(pointerIndex);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mActivePointerId == MotionEvent.INVALID_POINTER_ID) {
+                    return false;
+                }
+                pointerIndex = ev.findPointerIndex(mActivePointerId);
+                if (pointerIndex < 0) {
+                    return false;
+                }
+                final float x = ev.getX(pointerIndex);
+                final float y = ev.getY(pointerIndex);
+                startDragging(x, y);
+                break;
+
+            case MotionEventCompat.ACTION_POINTER_UP:
+                onSecondaryPointerUp(ev);
+                break;
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                isBeingDragged = false;
+                mActivePointerId = MotionEvent.INVALID_POINTER_ID;
+                break;
+        }
+        return isBeingDragged;
+    }
+
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (!isEnabled() || mContent == null || mHeader == null)
+            return false;
+        final int action = MotionEventCompat.getActionMasked(ev);
+        int pointerIndex;
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mActivePointerId = ev.getPointerId(0);
+                isBeingDragged = false;
+                break;
+            case MotionEvent.ACTION_MOVE: {
+                pointerIndex = ev.findPointerIndex(mActivePointerId);
+                if (pointerIndex < 0) {
+                    return false;
+                }
+                final float x = ev.getX(pointerIndex);
+                final float y = ev.getY(pointerIndex);
+                startDragging(x, y);
+                if (isBeingDragged) {
+                    final int offset = (int) ((y - mLastMotionY) / DRAGGING_RESISTANCE);
+                    final int targetOffset = mTotalOffset + offset;
+                    if (canRefresh && !isRefreshing && mUIHandler != null)
+                        mUIHandler.onPull(targetOffset >= mRefreshThreshold, targetOffset);
+                    mLastMotionY = y;
+                    if (targetOffset >= 0) {
+                        setOffsetTopAndBottom(offset);
+                    } else {
+                        setOffsetTopAndBottom(-mTotalOffset);
+                        return false;
+                    }
+                }
+                break;
+            }
+            case MotionEventCompat.ACTION_POINTER_DOWN: {
+                pointerIndex = MotionEventCompat.getActionIndex(ev);
+                if (pointerIndex < 0) {
+                    return false;
+                }
+                mActivePointerId = ev.getPointerId(pointerIndex);
+                mLastMotionY = ev.getY(pointerIndex);
+                mLastMotionX = ev.getX(pointerIndex);
+                break;
+            }
+
+            case MotionEventCompat.ACTION_POINTER_UP:
+                onSecondaryPointerUp(ev);
+                break;
+
+            case MotionEvent.ACTION_UP: {
+                pointerIndex = ev.findPointerIndex(mActivePointerId);
+                if (pointerIndex < 0) {
+                    return false;
+                }
+                if (isBeingDragged) {
+                    isBeingDragged = false;
+                    if (isRefreshing) {
+                        if (mTotalOffset > mHeaderHeight)
+                            animateOffsetToRefreshPosition(mTotalOffset);
+                    } else {
+                        if (canRefresh && mTotalOffset >= mRefreshThreshold) {
+                            animateOffsetToRefreshPosition(mTotalOffset);
+                            isRefreshing = true;
+                            if (mListener != null) mListener.onRefresh();
+                            if (mUIHandler != null) mUIHandler.onStart();
+                        } else {
+                            animateOffsetToStartPosition(mTotalOffset);
+                        }
+                    }
+                }
+                mActivePointerId = MotionEvent.INVALID_POINTER_ID;
+                return false;
+            }
+            case MotionEvent.ACTION_CANCEL:
+                return false;
+        }
+
+        return true;
+    }
+
+    private void startDragging(float x, float y) {
+        final float xDiff = x - mLastMotionX;
+        final float yDiff = y - mLastMotionY;
+        if (Math.abs(xDiff) < mTouchSlop && yDiff >= mTouchSlop && !isBeingDragged) {
+            mLastMotionY = mLastMotionY + (yDiff > 0 ? mTouchSlop : -mTouchSlop);
+            isBeingDragged = true;
+            if (canRefresh && mUIHandler != null) mUIHandler.onPrepare();
+        }
+    }
+
+    private void onSecondaryPointerUp(MotionEvent ev) {
+        final int pointerIndex = MotionEventCompat.getActionIndex(ev);
+        final int pointerId = ev.getPointerId(pointerIndex);
+        if (pointerId == mActivePointerId) {
+            final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+            mActivePointerId = ev.getPointerId(newPointerIndex);
+        }
+    }
+
+    private void setOffsetTopAndBottom(int offset) {
+        mTotalOffset += offset;
+        if (offset == 0) return;
+        mHeader.bringToFront();
+        ViewCompat.offsetTopAndBottom(mHeader, offset);
+        ViewCompat.offsetTopAndBottom(mContent, offset);
+        updateLayout();
+    }
+
+    private void animateOffsetToStartPosition(int from) {
+        mFrom = from;
+        long duration = (long) (250.0f * Math.abs(mFrom) / mHeaderHeight);
+        mAnimateToStartPosition.setDuration(duration);
+        clearAnimation();
+        startAnimation(mAnimateToStartPosition);
+    }
+
+    private void animateOffsetToRefreshPosition(int from) {
+        mFrom = from;
+        int distance = Math.abs(mFrom - mHeaderHeight);
+        long duration = (long) (150.0f * distance / mHeaderHeight);
+        mAnimateToRefreshPosition.setDuration(duration);
+        clearAnimation();
+        startAnimation(mAnimateToRefreshPosition);
+    }
+
+    private void moveToStart(float interpolatedTime) {
+        int targetTop = (mFrom + (int) (-mFrom * interpolatedTime));
+        int offset = targetTop - mContent.getTop();
+        setOffsetTopAndBottom(offset);
+    }
+
+    private void moveToRefresh(float interpolatedTime) {
+        int targetTop = (mFrom + (int) ((mHeaderHeight - mFrom) * interpolatedTime));
+        int offset = targetTop - mContent.getTop();
+        setOffsetTopAndBottom(offset);
+    }
+
+    public boolean canChildScrollUp() {
+        if (mContent == null) return false;
+        if (android.os.Build.VERSION.SDK_INT < 14) {
+            if (mContent instanceof AbsListView) {
+                final AbsListView absListView = (AbsListView) mContent;
+                return absListView.getChildCount() > 0
+                        && (absListView.getFirstVisiblePosition() > 0 || absListView.getChildAt(0)
+                        .getTop() < absListView.getPaddingTop());
+            } else {
+                return ViewCompat.canScrollVertically(mContent, -1) || mContent.getScrollY() > 0;
+            }
+        } else {
+            return ViewCompat.canScrollVertically(mContent, -1);
+        }
+    }
+
+    @Override
+    public void requestDisallowInterceptTouchEvent(boolean b) {
+        if ((android.os.Build.VERSION.SDK_INT < 21 && mContent != null && mContent instanceof AbsListView)
+                || (mContent != null && !ViewCompat.isNestedScrollingEnabled(mContent))) {
+        } else {
+            super.requestDisallowInterceptTouchEvent(b);
+        }
+    }
+
+    private boolean hasChild(View child) {
+        return indexOfChild(child) != -1;
+    }
+
+
+    public void setHeaderView(View header) {
+        if (mHeader == header) return;
+        if (this.mHeader != null && hasChild(header)) {
+            removeView(mHeader);
+        }
+        this.mHeader = header;
+        if (header == null) {
+            setUIHandler(null);
+            return;
+        }
+        if (header instanceof RefreshUIHandler)
+            setUIHandler((RefreshUIHandler) header);
+        else
+            throw new IllegalArgumentException("Header View must implements RefreshUIHandler!");
+        if (header.getLayoutParams() == null)
+            header.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        addView(header);
+    }
+
+    public void setContentView(View content) {
+        if (mContent == content) return;
+        if (this.mContent != null && hasChild(mContent)) {
+            removeView(mContent);
+        }
+        this.mContent = content;
+        if (content == null) return;
+        content.setOverScrollMode(OVER_SCROLL_NEVER);
+        content.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        addView(content);
+    }
+
+    public View getHeaderView() {
+        return mHeader;
+    }
+
+    public boolean isRefreshing() {
+        return isRefreshing;
+    }
+
+    public void setRefreshing(final boolean refreshing) {
+        if (!isRefreshing && refreshing) {
+            animateOffsetToRefreshPosition(mTotalOffset);
+            isRefreshing = true;
+            if (mUIHandler != null) mUIHandler.onStart();
+        } else if (isRefreshing && !refreshing) {
+            if (mUIHandler != null) mUIHandler.onComplete();
+            postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (!isBeingDragged) animateOffsetToStartPosition(mTotalOffset);
+                    isRefreshing = refreshing;
+                }
+            }, 500);
+        }
+
+    }
+
+    public void setUIHandler(RefreshUIHandler handler) {
+        this.mUIHandler = handler;
+    }
+
+    public void setOnRefreshListener(OnRefreshListener listener) {
+        this.mListener = listener;
+    }
+
+    public interface OnRefreshListener {
+        void onRefresh();
+    }
+
+    public interface RefreshUIHandler {
+
+        void onPrepare();
+
+        void onStart();
+
+        void onComplete();
+
+        void onPull(boolean willRefresh, int offset);
+
+    }
+}
